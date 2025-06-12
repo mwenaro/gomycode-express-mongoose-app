cd01a624721c3d95f89bc6392d5b22405aa95fac
Mwero Abdalla <mwerothewebmaker@gmail.com>
2025-06-12 22:11:40 +0300

    chore: add product route

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
 const express = require("express");
 import type { Response, Request } from "express";
+import { config } from "dotenv";
+import { dbConnect } from "./db/connect";
+import { productRouter } from "./routes/productRoute";
+config()
 
 //variables
 const PORT = process.env.PORT || 5000;
@@ -7,6 +11,15 @@ const PORT = process.env.PORT || 5000;
 //create app
 const app = express();
 
+//use meddilesraw
+app.use(express.json())
+
+//run db connection
+dbConnect()
+
+
+//products
+app.use("/products", productRouter)
 //routes
 app.get("/", (req: Request, res: Response) => {
   res.json({ msg: "Hello from server app" });
