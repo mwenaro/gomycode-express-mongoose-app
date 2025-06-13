f5980ed2c65c22e4a9d37d48dc6e1f32b06728fd
Mwero Abdalla <mwerothewebmaker@gmail.com>
2025-06-13 20:01:07 +0300

    chore: add dbConnect function for database connection

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import { dbConnect } from './db/dbConnect'
 
-
+//load env vars
+dotenv.config()
 //create variablrs
 const PORT  = process.env.PORT || 5000
 //create app
 const app = express()
-
+//run db connection
+dbConnect()
 //start app
 app.listen(PORT, ()=>console.log(`App is running on port ${PORT}`))
